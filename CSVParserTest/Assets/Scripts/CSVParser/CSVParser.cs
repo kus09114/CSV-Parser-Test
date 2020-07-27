354a9394ba7bab47ac0496b3d17b7c47996c11cd
kus09114 <66397141+kus09114@users.noreply.github.com>
2020-07-27 21:29:50 +0900

    CSV Parser Test

---
+++ CSVParserTest/Assets/Scripts/CSVParser/CSVParser.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSVParser
+{
+	// sPathName : 폴더가 Resources 하위부터 시작해야 한다. "TestData/test"
+    public static List<string[]> Load(string sPathName)
+	{
+		List<string[]> listData = new List<string[]>();
+
+		TextAsset kTextAsset = (TextAsset)Resources.Load(sPathName, typeof(TextAsset));
+		if (kTextAsset == null)
+			return null;
+
+		StringReader _reader = new StringReader(kTextAsset.text);
+
+		string inputData = _reader.ReadLine();
+		while(inputData != null)
+		{
+			string[] datas = inputData.Split('\t');
+			if (datas.Length == 0)
+				continue;
+
+			listData.Add(datas);
+
+			inputData = _reader.ReadLine();
+		}
+		_reader.Close();
+		return listData;
+	}
+
+
+
+
+
+	// sPathName : 주의) 폴더가 Assets부터 시작해야 한다. "Assets/Resources/TableData/test.csv"
+	public static List<string[]> Load2(string sPathName)
+	{
+		List<string[]> listData = new List<string[]>();
+
+		StreamReader sr = new StreamReader(sPathName, System.Text.Encoding.UTF8);
+		if (sr == null) return null;
+
+		while(!sr.EndOfStream)								// 스트림의 끝까지 읽기
+		{
+			string line = sr.ReadLine();					// 한 줄씩 읽어온다.
+
+			string[] datas = line.Split('\t');				// 탭을 기준으로 문자를 분리한다.
+			if (datas.Length == 0)
+				continue;
+
+			listData.Add(datas);
+		}
+		sr.Close();
+
+		return listData;
+	}
+}
---
+++ CSVParserTest/Assets/Scripts/CSVParser/ParserTestDlg.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParserTestDlg : MonoBehaviour
+{
+	public Text m_txtResult = null;
+	public Button m_btnLoad = null;
+	public Button m_btnParser = null;
+	public Button m_btnClear = null;
+
+    void Start()
+    {
+		m_btnLoad.onClick.AddListener(OnClick_BtnLoad);
+		m_btnParser.onClick.AddListener(OnClick_BtnParser);
+		m_btnClear.onClick.AddListener(OnClick_BtnClear);
+    }
+
+	public void Initialize()
+	{
+
+	}
+
+    void Update()
+    {
+        
+    }
+
+	public void OnClick_BtnLoad()
+	{
+		LoadingTest();
+	}
+	public void OnClick_BtnParser()
+	{ 
+ 
+	}
+	public void OnClick_BtnClear()
+	{
+
+	}
+
+	public void LoadingTest()
+	{
+		List<string[]> dataList = CSVParser.Load("TableData/test");
+		//List<string[]> dataList = CSVParser.Load2("Assets/Resources/TableData/test.csv");
+
+		string sResult = "";
+		for(int i = 0; i < dataList.Count; i++)
+		{
+			string[] data = dataList[i];
+			sResult += string.Format("{0} {1} {2}\n", data[0], data[1], data[2]);
+		}
+		m_txtResult.text = sResult;
+	}
+}
